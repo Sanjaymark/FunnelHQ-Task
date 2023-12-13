5be73574ae5f132afd24977ad49caea012f10fa6
Sanjaymark16 <sanjaymark16@gmail.com>
2023-12-13 19:18:27 +0530

    session

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.use(session({
   secret: sessionSecret,
   resave: false,
   saveUninitialized: true,
-  store: MongoStore.create({ mongooseConnection: mongoose.connection }),
+  store: new MongoStore.create({ mongooseConnection: mongoose.connection }),
 }));
 
 // Passport middleware
